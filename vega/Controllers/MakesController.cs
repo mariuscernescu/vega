95d7409d169b02b106480357d4baf9d586a31dde
Marius Cernescu <mariusc@ensemble.com>
2021-07-14 14:55:15 +0300

    created make api endpoint

---
+++ vega/Controllers/MakesController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistance;
+
+namespace vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly IMapper mapper;
+
+        private VegaDbContext _context { get; }
+        public MakesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+           var makes =  await _context.Makes.Include(m => m.Models).ToListAsync();
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
+        }
+    }
+}
---
+++ vega/Controllers/Resources/ModelResource.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace vega.Controllers.Resources
+{
+    public class ModelResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
